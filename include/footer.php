1ab9dbcb48c38779191d1710d0b766ffec90df01
sib-vishal <vishal.maurya@sibinfo.tech>
2024-06-06 16:42:32 +0530

    commit footer fixed

--- include/footer.php
+++ include/footer.php
@@ -1,68 +1,76 @@
 <footer class="footer ">
-	<div class="containerFull footer_inner ">
-		<div class="footer_logo">
-			<picture>
-				<source srcset="images/logo/logo_white.png" type="image/webp" />
-				<img src="images/logo/logo_white.png">
-			</picture>
-			<p class=" py-4 w-75">We bring the best digital marketing services to your doorstep. From SEO, SMM, web
-				design, web
-				development, to content marketing, Lorem has got you covered.</p>
-			<div class="socialIcons">
-				<ul class="d-flex">
-					<li><a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
-					</li>
-					<li><a href="https://twitter.com" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
-					</li>
-					<li><a href="https://www.linkedin.com/company" target="_blank"><i
-								class="fab fa-linkedin-in"></i></a></li>
-					<li><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
-				</ul>
+	<div class="containerFull text-white">
+		<div class="row">
+			<div class="col-12 col-xl-3 col-lg-12 col-md-6 d-flex flex-column lg-mb-30px sm-mb-20px order-md-1">
+				<div class="footer_logo">
+					<picture>
+						<source srcset="images/logo/logo_white.png" type="image/webp" />
+						<img src="images/logo/logo_white.png">
+					</picture>
+					<p class=" py-4 w-75 text-white">Pov Media offers comprehensive digital marketing solutions: SEO,
+						SMM, design,
+						development, and content.</p>
+					<div class="socialIcons">
+						<ul class="d-flex">
+							<li><a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+							</li>
+							<li><a href="https://twitter.com" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+							</li>
+							<li><a href="https://www.linkedin.com/company" target="_blank"><i
+										class="fab fa-linkedin-in"></i></a></li>
+							<li><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+							</li>
+						</ul>
+					</div>
+
+				</div>
 			</div>
+			<div class="col-12 col-xl-3 col-lg-4 col-md-6  order-lg-2 order-md-3 sm-mb-20px">
+				<span class="heading fw-700 d-block   text-uppercase">Address</span>
+				<p class="mt-2 text-white  small_heading "><strong class=" fw-600 mb-1"><i
+							class="fa-solid fa-location-dot"></i> &nbsp;Pov
+						Media </strong> <br>Goregaon (E), Mumbai.
+				</p>
+				<span class=" fw-600 d-block   text-uppercase mt-4 heading ">Contact Us</span>
+				<div class="mt-2 text-white">
 
-		</div>
-		<div class="footer_Links ">
-			<h4 class="text-white">Services</h4>
-			<ul>
-				<li><a href="#">Digital Marketing</a></li>
-				<li><a href="#">Social Media Management</a></li>
-				<li><a href="#">Content Marketing</a></li>
-				<!-- <li><a href="#">Search Engine Optimization Services </a></li>
-				<li><a href="#">Social Media Marketing Services</a></li>
-				<li><a href="#">Content Writing Services</a></li> -->
-			</ul>
-		</div>
-		<div class="footer_Links ">
-			<h4 class="text-white">Quick Links</h4>
-			<ul>
-				<li><a href="#">About Us</a></li>
-				<li><a href="#">Portfolio</a></li>
-				<li><a href="#">Case Studies
-					</a></li>
-				<li><a href="#">Our Clients</a></li>
-
-			</ul>
-		</div>
-		<div class="footer_Links ">
-			<!-- <h4 class="text-white">REACH US HERE</h4> -->
-			<div class="text-white footer_contact ">
-				<h4 class=" fs-14 fw-600 d-block  ">Work inquiries</h4>
-				<p class="mb-0 contact_link mt-1">Interested in working with us? </p>
-				<a href="mailto:contact@povmedia.in" class="  fw-600 d-block mt-1">contact@povmedia.in</a>
-				<h4 class="fs-14 fw-600 d-block   mt-4">Work with us</h4>
-				<p class="mb-0 contact_link mt-1">Looking for a job opportunity?</p>
-				<a href="mailto:hr@yourdomain.com" class=" fw-600 d-block mt-1 ">hr@povmedia.in</a>
+					<a href="tel:+91-9769245614 " class="text-white small_heading"> <i class="fa-solid fa-phone"></i>
+						+91-9769245614</a>
+				</div>
 			</div>
+			<div class="col-12 col-xl-3 col-lg-4 col-md-6  order-md-4 sm-mb-30px">
+				<span class=" fw-600 d-block   text-uppercase heading">Work inquiries</span>
+				<p class="mb-0 mt-1 text-white">Interested in working with us? </p>
+				<a href="mailto:contact@povmedia.in" class="  fw-600 mt-2 d-block text-white small_heading"><i
+						class="fa-solid fa-envelope"></i> contact@povmedia.in</a>
+				<p class=" fw-600 d-block   text-uppercase heading text-white mt-4">Work with us</p>
+				<p class="mb-0 text-white">Looking for a job opportunity?</p>
+				<!-- <a href="mailto:hr@povmedia.in" class="  fw-600 d-inline-block">hr@povmedia.in</a> -->
+				<a href="mailto:hr@povmedia.in" class="  fw-600 mt-2 d-block text-white small_heading"><i
+						class="fa-solid fa-envelope"></i> hr@povmedia.in</a>
+			</div>
+			<div class="col-12 col-lg-3 col-xl-3 col-lg-4 col-md-6 order-lg-4 order-md-2 md-mb-30px sm-mb-0">
+				<div class="footer_Links ">
+					<h4 class="text-white">Quick Links</h4>
+					<ul>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">Case Studies
+							</a></li>
+						<li><a href="#">Our Clients</a></li>
 
+					</ul>
+				</div>
+			</div>
 		</div>
-
 
 	</div>
+
 
 	<div class="copyRight">
 		<div class="containerFull">
 			<div class="copyRight_main">
-				<p>Copyright © <?php echo date('Y'); ?> Pov Media. All rights reserved<./p>
+				<p>Copyright © <?php echo date('Y'); ?> Pov Media. All rights reserved.</p>
 				<p>Website Design &amp; Development by <a href="https://www.sibinfotech.com" class="text-white"
 						target="_blank">SIB
 						Infotech</a></p>
